e30f3436deb880304bae17d9ffeeb46aca8f3eff
Remi Cazelles <remi.cazelles.fr@gmail.com>
2024-01-08 14:09:19 +0100

    configure la bdd sql avec SQLite

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+## Pour configurer la base de données avec SQLite
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')

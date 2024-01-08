4b577650ad5ac3da178ecd3120f47a2fe77cf2bf
Remi Cazelles <remi.cazelles.fr@gmail.com>
2024-01-08 14:10:54 +0100

    cleaner

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-## Pour configurer la base de données avec SQLite
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    # ...
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
